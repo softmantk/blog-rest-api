03f57d29753579d2c644be738473c5ae6345a13e
Nikhil CM <nikhil@innovatise.com>
2020-03-04 05:35:57 +0530

    initialized

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {BlogApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {BlogApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new BlogApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
